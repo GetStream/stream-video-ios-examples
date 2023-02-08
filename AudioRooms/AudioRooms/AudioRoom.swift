c2056e3a6e64a4b187de32bcd6c2ed58e69802a1
Stefan <stefan.blos@gmail.com>
2023-02-08 10:11:14 +0100

    Cleaned up AudioRoom and added preview extension

--- AudioRooms/AudioRooms/AudioRoom.swift
+++ AudioRooms/AudioRooms/AudioRoom.swift
@@ -15,21 +15,25 @@ struct AudioRoom: Identifiable {
     let hosts: [User]
 }
 
+extension AudioRoom {
+    static var preview: AudioRoom = DemoAudioRoomRepository.loadAudioRooms().first!
+}
+
 protocol AudioRoomRepository {
     
-    func loadAudioRooms() async -> [AudioRoom]
+    static func loadAudioRooms() -> [AudioRoom]
     
 }
 
 class DemoAudioRoomRepository: AudioRoomRepository {
     
-    let martin = User(id: "martin", name: "Martin", imageURL: URL(string: "https://getstream.io/static/2796a305dd07651fcceb4721a94f4505/802d2/martin-mitrevski.webp"), extraData: [:])
-    let tommaso = User(id: "tommaso", name: "Tommaso", imageURL: URL(string: "https://getstream.io/static/712bb5c0bd5ed8d3fa6e5842f6cfbeed/c59de/tommaso.webp"), extraData: [:])
-    let thierry = User(id: "thierry", name: "Thierry", imageURL: URL(string: "https://getstream.io/static/237f45f28690696ad8fff92726f45106/c59de/thierry.webp"), extraData: [:])
-    let filip = User(id: "filip", name: "Filip", imageURL: URL(string: "https://getstream.io/static/76cda49669be38b92306cfc93ca742f1/802d2/filip-babi%C4%87.webp"), extraData: [:])
-    let marcelo = User(id: "marcelo", name: "Marcelo", imageURL: URL(string: "https://getstream.io/static/aaf5fb17dcfd0a3dd885f62bd21b325a/802d2/marcelo-pires.webp"), extraData: [:])
+    private static let martin = UserCredentials.builtInUsersByID(id: "martin")!.userInfo
+    private static let tommaso = UserCredentials.builtInUsersByID(id: "tommaso")!.userInfo
+    private static let thierry = UserCredentials.builtInUsersByID(id: "thierry")!.userInfo
+    private static let filip = UserCredentials.builtInUsersByID(id: "filip")!.userInfo
+    private static let marcelo = UserCredentials.builtInUsersByID(id: "marcelo")!.userInfo
     
-    func loadAudioRooms() async -> [AudioRoom] {
+    static func loadAudioRooms() -> [AudioRoom] {
         let footballRoom = AudioRoom(
             id: "audioroom1",
             title: "The football room",
